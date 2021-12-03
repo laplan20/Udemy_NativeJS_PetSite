0fc65c72c08ebff30f251c38430af2826bf7e1b8
Pavel Biryukov <eqb-pavel@yandex.com>
2021-12-03 17:39:51 +1000

    Add form cyrillic sybmol validation. Add phone mask.

---
+++ src/js/modules/checkTextInputs.js
@@ -0,0 +1,18 @@
+const checkTextInputs = (selector) => {
+  const txtInputs = document.querySelectorAll(selector);
+
+  txtInputs.forEach((input) => {
+    input.addEventListener("keypress", function (event) {
+      if (e.key.match(/[^а-яё 0-9]/gi)) {
+        e.preventDefault();
+      }
+    });
+    input.addEventListener("input", () => {
+      if (input.value.match(/[a-z]/gi)) {
+        input.value = "";
+      }
+    });
+  });
+};
+
+export default checkTextInputs;
